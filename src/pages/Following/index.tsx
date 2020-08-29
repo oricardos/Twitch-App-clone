4b7d59eebfdb1b5e9544b96ead440b66f09ccbca
Luiz Ricardo Silva <ricardo.rs95@gmail.com>
2020-08-28 22:00:00 -0300

    :tada: commit initial

---
+++ src/pages/Following/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Wrapper, Container, Main } from './styles';
+
+const Following: React.FC = () => {
+  return (
+    <Wrapper>
+      <Container>
+        <Main />
+      </Container>
+    </Wrapper>
+  );
+};
+
+export default Following;
