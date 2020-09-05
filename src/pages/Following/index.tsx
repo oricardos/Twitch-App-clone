5650cf4825401b07e11fa47bb813dd03d878b151
Luiz Ricardo Silva <ricardo.rs95@gmail.com>
2020-09-05 00:03:20 -0300

    :construction: create Header

--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View } from 'react-native';
+import Header from '../../components/Header';
 
 import { Wrapper, Container, Main } from './styles';
 
+interface Item {
+  key: string;
+  render: () => JSX.Element;
+  isTitle?: boolean;
+}
+
 const Following: React.FC = () => {
+  React.useMemo(() => {
+    const items: Item[] = [
+      {
+        key: 'PAGE-HEADING',
+        render: () => <View />
+      }
+    ]
+  }, [])
+
   return (
     <Wrapper>
       <Container>
+        <Header />
         <Main />
       </Container>
     </Wrapper>
